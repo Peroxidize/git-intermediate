4729dd1119df2b1e88b89e548395ac40c74bf669
Peroxidize <marionemmanuelbuhat@gmail.com>
2024-12-03 16:54:17 +0800

    Refactor printHelloWorld function to take in a number that repeats printing n amount of times

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function printHelloWorld() {
-    console.log("Hellow World!");
+function printHelloWorld(num) {
+    for (let i = 0; i < num; i++) {
+        console.log("Hellow World!");
+    }
 }
 
 function subtract(a, b) {
